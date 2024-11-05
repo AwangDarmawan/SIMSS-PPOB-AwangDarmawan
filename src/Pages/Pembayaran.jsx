8a63d2f786a817a740a2a8025b317c3a789fb9fe
AwangDarmawan <adarmawan468@gmail.com>
2024-11-05 20:02:11 +0700

    bismilah

--- src/Pages/Pembayaran.jsx
+++ src/Pages/Pembayaran.jsx
@@ -3,20 +3,11 @@
 import CardPembayaran from '../Component/CardPembayaran'
 import Nav from '../Component/Nav'
 import CardHome from '../Component/CardHome'
-// import React, { useEffect, useState } from 'react'
-// import { getBalance } from '../Service/Api'
+
 
 
 function Pembayaran() {
-  // const [dataBalance, setDataBalance] = useState(0);
-  // const fetchBalance = async()=>{
-  // const nilai = await getBalance();
-  //   setDataBalance(nilai.data)
-  // }
-  
-  // useEffect(() => {
-  //   fetchBalance();
-  // }, []);
+ 
 
   return (
     <>
